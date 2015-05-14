9b5659d2657f873dda089db19eb48694ebb5715f
HJ <hj@HJdeMacBook-Pro-4.local>
2015-05-14 16:57:50 +0800

    fix bug

--- app/scripts/controllers/siderbar.js
+++ app/scripts/controllers/siderbar.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('managementApp', [])
-.controller('SiderbarController', function($scope, $http){
+ManagementApp.controller('SiderbarController', function($scope, $http){
 	$http.get('scripts/datas/classMsList.json').success(function(data){
 		$scope.classMsList = data.classMsList;
 		$scope.isShow = false;
--- app/scripts/directives/header.js
+++ app/scripts/directives/header.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('managementApp', [])
-.directive('msHeader', function(){
+ManagementApp.directive('msHeader', function(){
 	return{
 		restrict:'E',
 		templateUrl:'views/header.html',
