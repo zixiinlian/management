abf3df4889875698c3b5399f53c607223db8409c
Will Duan <dqywill@outlook.com>
2015-05-18 16:46:47 +0800

    add BackendDotNet solution

---
+++ src/WebApplicationForTest/Test.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="WebApplicationForTest.Test" %>
+
+<script type="text/ng-template">
+<div>
+    <% foreach (var member in Members)
+       { %>
+    <ul>
+        <li>名称：<%= member.Name %></li>
+        <li>年龄：<%= member.Age %></li>
+        <li>国家：<%= member.Country %></li>
+    </ul>
+    <% } %>
+</div>
+</script>
---
+++ src/WebApplicationForTest/Test.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationForTest
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected List<UserMember> Members { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int age;
+            int.TryParse(Request.QueryString["age"], out age);
+            Members = GetMembers();
+            if (age != 0)
+            {
+                Members = Members.Where(m => m.Age > age).ToList();
+            }
+        }
+
+        private List<UserMember> GetMembers()
+        {
+            return new List<UserMember>{
+                new UserMember{
+                    Name = "张三",
+                    Age = 38,
+                    Country = "中国"
+                },
+                new UserMember{
+                    Name = "李四",
+                    Age = 37,
+                    Country = "中国"
+                },
+                new UserMember{
+                    Name = "Lily",
+                    Age = 20,
+                    Country = "US"
+                },
+                new UserMember{
+                    Name = "Lucy",
+                    Age = 22,
+                    Country = "US"
+                }
+            };
+        }
+    }
+
+    public class UserMember
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+    }
+}
