2b876bb43272ada05940fadf18478cd6c0286440
dihaibo <dihaibo@hujiang.com>
2015-05-18 18:21:24 +0800

    qw

--- app/scripts/controllers/siderbar.js
+++ app/scripts/controllers/siderbar.js
@@ -4,7 +4,7 @@ ManagementApp.controller('SiderbarController', function($scope, $http){
 	$http.get('scripts/datas/classMsList.json').success(function(data){
 		$scope.classMsList = data.classMsList;
 		$scope.isShow = false;
-
+		
 		$scope.toggleNav = function(){
 			console.log(this.element);
 		};
--- app/scripts/directives/treeNav.js
+++ app/scripts/directives/treeNav.js
@@ -1,12 +1,17 @@
-'use strict';
+//'use strict';
 
-ManagementApp.directive('treeNav', function(){
+ManagementApp.directive('treenav', function(){
+	
 	return{
 		restrict:'E',
-		templateUrl:'views/treeNav.html',
+//		templateUrl:'views/treeNav.html',
 		replace: true,
 		transclude: false,
 		scope: false,
 		controller:'infoList'
+		//setDomModel(,$('.info-tab'))
+//		template:$scope.setDomModel($scope.infoList,$('.info-tab'))
+		
+		
 	};
 });
