755155f2b0f18fc68216491b8fb8b67b9162dce8
dihaibo <dihaibo@hujiang.com>
2015-05-19 13:59:28 +0800

    aksdk

--- app/scripts/controllers/infoList.js
+++ app/scripts/controllers/infoList.js
@@ -3,25 +3,42 @@ ManagementApp.controller('infoList',function($scope,$http){
 	$http.get('scripts/datas/infoList.json')
 	.success(function(data){
 		$scope.infoList = data.list;
-		$scope.setDomModel($scope.infoList,$('.info-tab'))
+		$scope.setDomModel($scope.infoList,$('.info-tab .panel-body'));
+		$scope.toggleFn()
 	})
 	$scope.setDomModel = function(list, parent){
-		var $parent = $('<ul/>');
+		var liInner;
 			for (var i in list) {
-				var $li = $('<li/>');
+				var $li = $('<li></li>');
 				for (var j in list[i]) {
 					if (list[i][j] instanceof Object) {
-						var $ul = $('<ul/>');
+						var $ul = $('<ul class="subList"></ul>');
 						$ul.appendTo($li);
 						arguments.callee(list[i][j], $ul)
 					} else {
-						$li.append('<a href="'+list[i].url+'">'+list[i][j]+'</a>').appendTo(parent)
+						if(list[i].url){
+							liInner = '<a href="'+list[i].url+'">'+list[i][j]+'</a>'
+						}else{
+							liInner = '<span>' + list[i][j] + '</span>'
+						}
+						$li.append(liInner).appendTo(parent)
 						
 					}
 				}
-				$parent.append($li)
 			}
-			parent.append($parent)
+	}
+	$scope.toggleFn = function(){
+//		$('.tree .subList').hide();
+		$('.tree span').click(function(){
+			$(this).next('.subList').show();
+			$(this).parent().siblings().children('.subList').hide();
+			if($(this).next().css('display') == 'block'){
+				
+			}
+			
+			
+		})
+		
 	}
 	
 	
