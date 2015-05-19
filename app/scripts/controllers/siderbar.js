32a773970d35db92e85fba0233ca2f41c073b357
HJ <835898749@qq.com>
2015-05-19 13:57:55 +0800

    style

--- app/scripts/controllers/siderbar.js
+++ app/scripts/controllers/siderbar.js
@@ -4,9 +4,11 @@ ManagementApp.controller('SiderbarController', function($scope, $http){
 	$http.get('scripts/datas/classMsList.json').success(function(data){
 		$scope.classMsList = data.classMsList;
 		$scope.isShow = false;
+		$scope.isCurrent = false;
 		
-		$scope.toggleNav = function(){
-			console.log(this.element);
+		$scope.setActive = function(){
+			$scope.isCurrent = true;
 		};
+
 	});
 });
